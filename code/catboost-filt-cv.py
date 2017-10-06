dc822e1c39ec2a281827d53cfcf7b5ca7406f1a5
Yan <astromid@users.noreply.github.com>
2017-10-06 19:20:48 +0300

    catboost-filt-cv: class weights

--- code/catboost-filt-cv.py
+++ code/catboost-filt-cv.py
@@ -15,7 +15,7 @@ id_test = np.load(data_path + 'id-test.npy')
 X_test = np.load(data_path + 'X-test-filt.npy')
 feat_names = np.load(data_path + 'feat_names.npy')
 
-model = CatBoostClassifier()
+model = CatBoostClassifier(class_weights=[0.5, 5.0])
 model.fit(X, y, verbose=True)
 
 order = np.argsort(model._feature_importance)
@@ -27,7 +27,7 @@ plt.tick_params(labelsize=16)
 plt.xlim([0.1, max(model._feature_importance)*1.5])
 plt.ylim(-1, len(order))
 plt.xscale('log')
-plt.savefig('feature-imp-fe-filt.png')
+plt.savefig('feature-imp-fe-filt-w.png')
 print('Feature importance saved')
 
 p_test = model.predict_proba(X_test)[:, 1]
@@ -35,5 +35,5 @@ p_test = model.predict_proba(X_test)[:, 1]
 sub_df = pd.DataFrame()
 sub_df['id'] = id_test
 sub_df['target'] = p_test
-sub_df.to_csv(sub_path + 'catb-filt-fe-1.csv', index=False)
+sub_df.to_csv(sub_path + 'catb-filt-fe-w-1.csv', index=False)
 print('Submission file created')
