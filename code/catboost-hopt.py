00469d080ed137efe04bfa112c4b4e551722a137
Yan <astromid@users.noreply.github.com>
2017-10-06 23:10:03 +0300

    hopt

--- code/catboost-hopt.py
+++ code/catboost-hopt.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
 from catboost import CatBoostClassifier
-# from sklearn.model_selection
+from hyperopt import hp, fmin, tpe, Trials, space_eval
+from sklearn.model_selection import KFold, cross_val_score
 
 data_path = '../data/'
 sub_path = '../subs/'
@@ -11,7 +12,28 @@ y = np.load(data_path + 'y.npy')
 id_test = np.load(data_path + 'id-test.npy')
 X_test = np.load(data_path + 'X-test-filt.npy')
 
-model = CatBoostClassifier()
+space = {}
+space['iteration'] = 100 + hp.randint('iteration', 901)
+space['depth'] = 3 + hp.randint('depth', 8)
+space['lr'] = hp.choice('lr', [0.01, 0.03, 0.1])
+space['l2_leaf_reg'] = 1 + hp.randint('l2_leaf_reg', 5)
+space['class_weight'] = hp.choice('class_weight', [[1, 1], [0.5, 1.5], [1, 5], [1, 10], [5, 1], [10, 1]])
+
+def hopt_objective(params):
+    print('Current params:')
+    print(params)
+    est = CatBoostClassifier(params)
+    shuffle = KFold(n_splits=5, shuffle=True)
+    score = cross_val_score(est, X, y, cv=shuffle, scoring='roc-auc', n_jobs=4, verbose=1)
+    return 1-score.mean()
+
+trials = Trials()
+best = fmin(hopt_objective, space, algo=tpe.suggest, max_evals=200, trials=trials)
+
+best_params = space_eval(space, best)
+np.save('best-params.npy', best_params)
+print(best_params)
+model = CatBoostClassifier(best_params)
 model.fit(X, y, verbose=True)
 np.save('../pics/feat-imp.npy', model._feature_importance)
 p_test = model.predict_proba(X_test)[:, 1]
@@ -19,5 +41,5 @@ p_test = model.predict_proba(X_test)[:, 1]
 sub_df = pd.DataFrame()
 sub_df['id'] = id_test
 sub_df['target'] = p_test
-sub_df.to_csv(sub_path + 'catb-filt-fe-2.csv', index=False)
+sub_df.to_csv(sub_path + 'catb-hopt.csv', index=False)
 print('Submission file created')
--- code/feat-imp.py
+++ code/feat-imp.py
@@ -11,7 +11,7 @@ order = np.argsort(feat_imp)
 plt.figure(figsize=[10, 15])
 plt.plot(np.array(feat_imp)[order], range(len(order)), marker='o')
 plt.hlines(range(len(order)), np.zeros_like(order), np.array(feat_imp)[order], linestyles=':')
-plt.yticks(range(X.shape[1]), feat_names[order]);
+plt.yticks(range(X_shape1), feat_names[order]);
 plt.tick_params(labelsize=16)
 plt.xlim([0.1, max(feat_imp)*1.5])
 plt.ylim(-1, len(order))
