36d0d27554f6089a38c52c5e4cf634dabe827f6d
astromid <astromid@gmail.com>
2017-10-12 10:27:04 +0300

    (2) catboost

--- code/catboost-base.py
+++ code/catboost-base.py
@@ -4,16 +4,13 @@ from catboost import CatBoostClassifier
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 sub_path = '../subs/'
-dir_name = 'fe1-npy'
+dir_name = 'raw-npy'
 
 X = np.load('../data/' + dir_name + '/X.npy')
 y = np.load('../data/' + dir_name + '/y.npy')
 
 hopt_params = np.load('best-params.npy').item()
-cat_feats = ['ps_ind_02_cat', 'ps_ind_04_cat', 'ps_ind_05_cat', 'ps_car_01_cat',
-             'ps_car_02_cat', 'ps_car_03_cat', 'ps_car_04_cat', 'ps_car_04_cat',
-             'ps_car_04_cat', 'ps_car_05_cat', 'ps_car_06_cat', 'ps_car_07_cat',
-             'ps_car_08_cat', 'ps_car_09_cat', 'ps_car_10_cat', 'ps_car_11_cat']
+cat_feats = [1, 3, 4, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31]
 fit_params = {'cat_features' : cat_feats}
 model = CatBoostClassifier(**hopt_params)
 skf = StratifiedKFold(n_splits=7, shuffle=True, random_state=42)
@@ -31,6 +28,6 @@ id_test = np.load('../data/' + dir_name + '/id-test.npy')
 sub_df['id'] = id_test
 sub_df['target'] = p_test
 sub_name = str(mean_cv)
-sub_df.to_csv(sub_path + 'catb-1-' + sub_name + '.csv', index=False)
+sub_df.to_csv(sub_path + 'catb-2-' + sub_name + '.csv', index=False)
 print('mean cv score: ', mean_cv)
 print('Submission file created')
